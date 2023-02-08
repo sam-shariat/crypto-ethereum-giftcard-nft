0193d5e61cc0e4f0b28d734a5c4f08263bd0256c
Sam Shariat <samshariat7@gmail.com>
2023-02-08 19:59:11 +0330

    Updates

---
+++ assets/ButtonTranslucent-f137031f.js
@@ -0,0 +1,16 @@
+import{v as r,s,t,w as a,x as e}from"./index-bd206bbf.js";import"./openlogin.esm-3de162c4.js";import"./base.esm-e36f3d2a.js";import"./elliptic-75fd6b2c.js";import"./index-2a7c4012.js";import"./index-346e2c9d.js";import"./url-5da867a5.js";import"./index-2a1b3d9a.js";const n=r(e)`
+    background-color: ${s("dark",20)};
+    color: ${t.white};
+
+    :active {
+        border: 2px solid transparent;
+    }
+
+    :focus {
+        box-shadow: 0px 0px 0px 2px ${t.paleCerulean};
+    }
+
+    svg {
+        fill: ${t.white};
+    }
+`;var l={ButtonTranslucentStyled:n};const{ButtonTranslucentStyled:p}=l,y=({...o})=>a(p,{...o});export{y as default};
