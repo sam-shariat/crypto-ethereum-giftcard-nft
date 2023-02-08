b03e483e33c5372a3e23633121c80482f404a1c7
Sam Shariat <samshariat7@gmail.com>
2023-02-08 19:37:52 +0330

    Updates

---
+++ assets/ButtonOutline2-6671565c.js
@@ -0,0 +1,27 @@
+import{E as r,D as o,J as e,G as l,H as u}from"./index-45f412d8.js";import"./openlogin.esm-ab039618.js";import"./base.esm-a6e8b20d.js";import"./elliptic-aa16d8cf.js";import"./index-66434860.js";import"./index-50a7582d.js";import"./url-4a71cc4c.js";import"./index-2a1b3d9a.js";import"./_commonjs-dynamic-modules-302442b1.js";const a=r(u)`
+    background-color: ${o.white};
+    border-color: ${o.beauBlue};
+    color: ${o.blue};
+
+    :hover {
+        background-color: ${e.beauBlue};
+        border-color: transparent;
+        color: ${o.blue};
+
+        svg {
+            color: ${o.blue};
+        }
+    }
+
+    :active {
+        box-shadow: 0px 0px 0px 2px ${o.blueDark};
+    }
+
+    :focus {
+        box-shadow: 0px 0px 0px 2px ${o.paleCerulean};
+    }
+
+    svg {
+        color: ${o.blue};
+    }
+`;var s={ButtonOutlineStyled:a};const{ButtonOutlineStyled:p}=s,g=({...t})=>l(p,{...t});export{g as default};
