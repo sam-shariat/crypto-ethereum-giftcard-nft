2df1fd14bccf30eab9a2bcb0cbac4a021baa7824
Sam Shariat <samshariat7@gmail.com>
2023-02-10 00:04:20 +0330

    Updates

--- assets/ButtonColored-7591be29.js
+++ assets/ButtonColored-7591be29.js
@@ -1,4 +1,4 @@
-import{y as r,z as l,A as o,D as a,E as n,G as s}from"./index-8c8e9323.js";import"./openlogin.esm-203447d9.js";import"./base.esm-5626fa93.js";import"./elliptic-bf52e6d2.js";import"./index-14760aac.js";import"./index-e21ba62e.js";import"./url-a89cacc9.js";import"./index-2a1b3d9a.js";import"./commonjs-dynamic-modules-302442b1.js";const t=r`
+import{o as r,q as l,s as o,t as a,v as n,w as s}from"./index-01e8a1b2.js";import"./openlogin.esm-203447d9.js";import"./base.esm-5626fa93.js";import"./elliptic-bf52e6d2.js";import"./index-14760aac.js";import"./index-e21ba62e.js";import"./url-a89cacc9.js";import"./index-2a1b3d9a.js";import"./_commonjs-dynamic-modules-302442b1.js";const t=r`
     :after {
         background-color: ${l("light",90)};
     }
